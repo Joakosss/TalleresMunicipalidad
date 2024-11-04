44e79727e4f4498f3205ebfcaa2dbcabf76ce729
joaquin <jo.perezu@duocuc.cl>
2024-11-03 21:15:27 -0300

    login y colores

--- talleresAPP/views.py
+++ talleresAPP/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login(request):
+    return render(request, 'pages/login.html')
+
+def register(request):
+    return render(request, 'pages/register.html')
+
 def index(request):
     return render(request, 'pages/index.html')
 
