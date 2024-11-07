22227d0a651f3ba6574e79eda589e881c5c68227
joaquin <jo.perezu@duocuc.cl>
2024-11-07 16:52:28 -0300

    crud taller en views index y talleres

--- talleresAPP/views.py
+++ talleresAPP/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 def login(request):
@@ -9,7 +9,11 @@ def register(request):
     return render(request, 'pages/register.html')
 
 def index(request):
-    return render(request, 'pages/index.html')
+    #se hace una consulta a talleres con un join a instructor
+    lisTalleres = models.Taller.objects.select_related('instructor').all()
+    return render(request, 'pages/index.html', {'talleres': lisTalleres})
 
-def talleres(request):
-    return render(request, 'pages/talleres.html')
+def talleres(request): 
+    #se hace una consulta a talleres con un join a instructor,sala y municipalidad
+    lisTalleres = models.Taller.objects.select_related('instructor','sala','municipalidad').all()
+    return render(request, 'pages/talleres.html',{'talleres': lisTalleres})
