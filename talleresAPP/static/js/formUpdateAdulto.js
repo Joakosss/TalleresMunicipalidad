125330064e2ee863433b427d48b9f3d39aff0362
joaquin <jo.perezu@duocuc.cl>
2024-11-22 09:52:13 -0300

    comienzo validaciones js

--- talleresAPP/static/js/formUpdateAdulto.js
+++ talleresAPP/static/js/formUpdateAdulto.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+    const regionSelect = this.getElementById("region");
+    const comunaSelect = this.getElementById("comuna");
+
+    regionSelect.addEventListener('change', function() {
+        const regionId = this.value;
+        if (regionId) {
+            fetch(`/get_comunas/?region_id=${regionId}`).then(response => response.json()).then(data => {
+                comunaSelect.innerHTML = '<option value="" disabled selected>Seleccione una comuna</option>';
+                data.forEach(comuna => {
+                    const option = document.createElement('option');
+                    option.value = comuna.id;
+                    option.textContent = comuna.nombre;
+                    comunaSelect.appendChild(option);
+                });
+            });
+        } else{
+            comunaSelect.innerHTML = '<option value="">Seleccione una comuna</option>';
+        };
+    });
+
 });
+
+
+/* Funcion de validaciones que se reutiliza durante el proceso del formulario */
+function validaciones(errorDoom, condicion){
+    if (condicion) {
+      errorDoom.classList.remove("d-none");
+    } else {
+      errorDoom.classList.add("d-none");
+    }
+  }
