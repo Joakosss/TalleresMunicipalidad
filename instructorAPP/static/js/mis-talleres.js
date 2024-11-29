b2b51771e36f34c4bb14418bc6162c2ddc0efc29
joaquin <jo.perezu@duocuc.cl>
2024-11-29 18:54:55 -0300

    mis talleres 100%

--- instructorAPP/static/js/mis-talleres.js
+++ instructorAPP/static/js/mis-talleres.js
@@ -1,25 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const articlePrincipal = document.getElementById("articleMisTalleres");
-
-});
-
 function misParticipantes(id_taller){
-    const participantes = fetch(`get-participantes/${id_taller}`)
+    let modalBody = document.getElementById("modal-body-id");
+    modalBody.innerHTML = `<div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
+                           </div>`;
+
+    fetch(`get-participantes/${id_taller}`)
     .then(response=> response.json())
     .then(data=>{
-        if (Array.isArray()) {
-            data.forEach(element => {
-                /* document.getElementById("articleMisTalleres").innerHtml = mostrarParticipantes(element); */
-                console.log(element);
-            });
+        modalBody.innerHTML = '';
+        modalBody.innerHTML = `<h5>Participantes</h5>`;
+        const participantes = Array.isArray(data) ? data : [data];
+
+        if (participantes.length === 0) {
+            modalBody.innerHTML += `<p>No hay participantes inscritos</p>`;
         } else {
-            /* document.getElementById("articleMisTalleres").innerHtml = mostrarParticipantes(data); */
-            console.log(data);
+            participantes.forEach(element => {
+                modalBody.innerHTML += mostrarParticipantes(element);
+            });
         }
     });
 };
 
 function mostrarParticipantes(data){
-    return `<p>${data}Hola</p>`;
+    return `<p>Nombre:<strong> ${data.nombre_adulto_mayor} </strong></p>`;
 }
 
--- instructorAPP/views.py
+++ instructorAPP/views.py
@@ -19,6 +19,16 @@ def misTalleres(request):
 #Ajax
 
 def participantesTaller(request, id_taller):
-    participantes = models.TallerAdultoMayor.objects.filter(taller_id=id_taller).select_related('adultomayor')
-    lista_participantes = list(participantes.values())
+    participantes = models.TallerAdultoMayor.objects.filter(taller_id=id_taller).select_related('adulto_mayor')
+    lista_participantes = [
+        {
+            'id': p.id,
+            'fecha_inicio': p.fecha_inicio,
+            'fecha_fin': p.fecha_fin,
+            'adulto_mayor_id': p.adulto_mayor_id,
+            'taller_id': p.taller_id,
+            'nombre_adulto_mayor': p.adulto_mayor.p_nombre +' '+ p.adulto_mayor.s_nombre +' '+p.adulto_mayor.p_apellido+' ' + p.adulto_mayor.s_apellido    # Asegúrate de que 'nombre' es el campo correcto
+        }
+        for p in participantes
+    ]
     return JsonResponse(lista_participantes, safe=False)
