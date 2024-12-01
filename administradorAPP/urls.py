bf37ec0e01a152cd78f82b4a7a202ac9c7ded621
CarolPachecoPozo <caro.pacheco@duocuc.cl>
2024-12-01 19:50:38 -0300

    paginas funcionario/admin

--- administradorAPP/urls.py
+++ administradorAPP/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path;
 from . import views;
 urlpatterns = [
+    path('', views.indexAdministrador, name='index_admin'),
+    path('pago', views.pago, name='pago'),
+    path('postulaciones', views.postulaciones, name='postulaciones'),
+    path('perfil-admin', views.perfilAdmin, name='perfil_admin'),
 ]   
--- administradorAPP/views.py
+++ administradorAPP/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexAdministrador(request):
+    return render(request, 'pages/index-admin.html')
+
+def pago(request):
+    return render(request, 'pages/pago.html')
+
+def postulaciones(request):
+    return render(request, 'pages/postulaciones.html')
+
+def perfilAdmin(request):
+    return render(request, 'pages/perfil-admin.html')
